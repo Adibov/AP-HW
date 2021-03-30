66cbd79447b57a5072d3adda208ee9c2d3021212
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-03-31 02:37:23 +0430

    Add AP 2/Problem4

---
+++ AP 2/Problem4/LinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * LinkedList implementation Class
+ * @author Adibov
+ * @version 1.0
+ */
+public class LinkedList {
+    private int size;
+    private Node head;
+
+    /**
+     * simple constructor for making a empty list
+     */
+    public LinkedList() {
+        size = 0;
+        head = new Node();
+    }
+
+    /**
+     * add input to the end of list
+     * @param k
+     */
+    public void add(int k) {
+
+    }
+
+    /**
+     * main method, make an object and add some integers to it just for testing
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
