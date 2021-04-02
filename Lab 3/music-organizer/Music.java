56534385d0c3601c3958561ef09cc32a6fc89b12
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-04-02 21:00:31 +0430

    Create Music.java

---
+++ Lab 3/music-organizer/Music.java
@@ -0,0 +1,16 @@
+/**
+ * Music class for implementing music track
+ */
+public class Music {
+    String type, address;
+
+    /**
+     * constructor for making a new object
+     * @param type
+     * @param address
+     */
+    public Music(String type, String address) {
+        this.type = type;
+        this.address = address;
+    }
+}
