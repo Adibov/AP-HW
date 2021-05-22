fc4010c178be0cb250f8c48cd2f29b886a2a9d33
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-05-22 19:35:16 +0430

    Add Lab 8

---
+++ Lab 8/SimpleNotepad/SimpleNotepad/src/Client.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Client class, to simulate client behaviours
+ * @author Adibov
+ * @version 1.0
+ */
+public class Client {
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private Scanner inputScanner = new Scanner(System.in);
+
+    /**
+     * class constructor
+     */
+    public Client() {
+        try {
+            socket = new Socket("localhost", 2021);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * main method, makes class runnable
+     * @param args program args
+     */
+    public static void main(String[] args) {
+        Client client = new Client();
+        while (true) {
+            client.inputMessage();
+            try {
+                Thread.sleep(300);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            client.update();
+        }
+    }
+
+    /**
+     * input a new message from the client
+     */
+    public void inputMessage() {
+        while (true) {
+            String message = inputScanner.nextLine();
+            if (message.equals("over"))
+                break;
+            try {
+                System.out.println("Client sent this message: " + message);
+                out.writeObject(message);
+            }
+            catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * update client output stream if any message has been sent via the server
+     */
+    public void update() {
+        try {
+            String message = "";
+            while (true) {
+                message = (String) in.readObject();
+                System.out.println(message);
+            }
+        }
+        catch (IOException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ Lab 8/SimpleNotepad/SimpleNotepad/src/Server.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Server class, runs server and response to client
+ * @author Adibov
+ * @version 1.0
+ */
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private String message = "";
+
+    /**
+     * class constructor
+     */
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(2021);
+            socket = serverSocket.accept();
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * main method, make class runnable
+     * @param args program args
+     */
+    public static void main(String[] args) {
+        Server server = new Server();
+        while (true) {
+            server.setMessage(server.getInput());
+            server.printMessage();
+        }
+    }
+
+    /**
+     * getInput from the client
+     * @return result message
+     */
+    public String getInput() {
+        String receivedMessage = "", newMessage = "";
+        try {
+            while (true) {
+                newMessage = (String) in.readObject();
+                System.out.println("Server got this message: " + newMessage);
+                if (newMessage == "")
+                    break;
+                receivedMessage += newMessage;
+            }
+        }
+        catch (IOException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+        return receivedMessage;
+    }
+
+    /**
+     * write current message to the output stream
+     */
+    public void printMessage() {
+        System.out.println("Server wants to send this message: " + message);
+        try {
+            out.writeObject(message);
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * message getter
+     * @return message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * message setter
+     * @param message new message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
