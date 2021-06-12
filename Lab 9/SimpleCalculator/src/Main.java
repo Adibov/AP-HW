103f4acdfcf7729b866ad95720821359a3801196
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-06-12 17:53:55 +0430

    init JavaFx Main code

---
+++ Lab 9/SimpleCalculator/src/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader ( getClass().getResource( "SimpleCalculator.fxml" ));
+            loader.setController(new Controller());
+            Parent root = loader.load();
+            primaryStage.setScene(new Scene(root, 351, 410));
+            primaryStage.show();
+        }
+        catch (IOException exception) {
+//            exception.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
